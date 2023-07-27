8bdbecd22520cd271269612c69a241734dc8f535
컨트롤D <controld3785@gmail.com>
2023-07-27 17:39:32 +0900

    Play WYSI Video

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,11 @@ Number.prototype.fillZero = function(width){
 	return n.length >= width ? n:new Array(width-n.length+1).join('0') + n;//남는 길이만큼 0으로 채움
 }
 
+var ytAPIReady = false;
+function onYouTubeIframeAPIReady() {
+	ytAPIReady = true;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 	const countDownTimer = function (id, date) {
 		var _vDate = new Date(date); // 전달 받은 일자
@@ -12,14 +17,16 @@ document.addEventListener("DOMContentLoaded", () => {
 		var _day = _hour * 24;
 		var timer;
 
-		var text = document.getElementById(id);
+		var text = document.querySelector("." + id);
 		function showRemaining() {
 			var now = new Date();
 			var distDt = _vDate - now;
 
 			if (distDt <= 0) {
-				text.textContent = '와! 생일!';
-				return;
+				text.textContent = 'WYSI!'
+				text.id = 'counter-done'
+				setTimeout(loadYT, 0)
+				return
 			}
 
 			var days = Math.floor(distDt / _day);
@@ -39,6 +46,69 @@ document.addEventListener("DOMContentLoaded", () => {
 
 		requestAnimationFrame(showRemaining);
 	}
+
+	var videoContainer = document.getElementById('video')
+	function loadYT() {
+		console.log("Trying to load YouTube...")
+		
+		if (ytAPIReady) {
+			console.log("YT Ready!")
+
+			var player = new YT.Player('youtube-player', {
+				width: '1280', height: '720',
+				videoId: '1qBWa6dWBRk',
+				events: {
+				  'onReady': onPlayerReady,
+				  'onStateChange': onPlayerStateChange
+				}
+		  	});
+
+			function playVideo(event) {
+				event.target.playVideo();
+				setTimeout(() => {
+					console.log(event)
+					if (event.data != YT.PlayerState.PLAYING) {
+						console.log("RECALL VIDEO")
+						setTimeout(playVideo, 0, event);
+					}
+				}, 1000);
+			}
+
+			function onPlayerReady(event) {
+				event.target.setVolume(50)
+				event.target.playVideo()
+				setTimeout(() => {
+					if (event.data != YT.PlayerState.PLAYING) {
+						setTimeout(playVideo, 0, event);
+					}
+				}, 1000);
+			}
+
+			var done = false;
+			function onPlayerStateChange(event) {
+				if (event.data == YT.PlayerState.PLAYING && !done) {
+					setTimeout(stopVideo, 200 * 1000);
+					done = true;
+				}
+			}
+			
+			function stopVideo() {
+				player.stopVideo()
+			}
+		} else {
+			requestAnimationFrame(loadYT);
+		}
+	}
+
+	function setupYouTube() {
+		var tag = document.createElement('script');
+
+    	tag.src = "https://www.youtube.com/iframe_api";
+    	var firstScriptTag = document.getElementsByTagName('script')[0];
+    	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+	}
+
+	setupYouTube();
 
 	var dateObj = new Date();
 	dateObj.setDate(dateObj.getDate() + 1);
